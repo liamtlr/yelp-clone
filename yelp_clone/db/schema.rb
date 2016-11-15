d696c30513440190fb3216b0147c4c5e049817af
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-15 10:36:57 +0000

    1) Created vlaidation of uniqueness of restaurant name 2) Created failing test for validation of review rating between 1-5

--- yelp_clone/db/schema.rb
+++ yelp_clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115085944) do
+ActiveRecord::Schema.define(version: 20161115085600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,11 +26,8 @@ ActiveRecord::Schema.define(version: 20161115085944) do
   create_table "reviews", force: :cascade do |t|
     t.text     "thoughts"
     t.integer  "rating"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "restaurant_id"
-    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "reviews", "restaurants"
 end
--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -9,7 +9,13 @@ describe Restaurant, type: :model do
 
   it 'is not valid unless it has a unique name' do
     Restaurant.create(name: "Moe's Tavern")
-    restaurant = Restaurant.new("Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
     expect(restaurant).to have(1).error_on(:name)
   end
+
+  it 'is not valid if the rating is above 5 or below 1' do
+    review = Review.new(rating: 17)
+    expect(review).to have(1).error_on(:rating)
+  end
+  
 end
