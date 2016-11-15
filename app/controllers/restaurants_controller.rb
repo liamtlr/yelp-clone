f209246bd782f3cc7200d889860bbcb4595d51f0
Tomy8s <tomy8s@hotmail.com>
2016-11-15 12:39:39 +0000

    rearranged folder structure

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,39 @@
+class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		Restaurant.create(restaurant_params)
+		redirect_to restaurants_path
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(restaurant_params)
+		redirect_to restaurants_path
+	end
+
+	def destroy
+		Restaurant.find(params[:id]).destroy
+		redirect_to restaurants_path
+	end
+
+	private
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description, :rating)
+	end
+
+end
---
+++ spec/test_helper.rb
@@ -0,0 +1,13 @@
+def add_restaurant_and_return
+  visit '/restaurants'
+  click_link 'Add a restaurant'
+  fill_in :name, with: 'My restaurant'
+  fill_in :description, with: 'A great place to eat'
+  fill_in :rating, with: 5
+  click_button 'Add Restaurant'
+end
+
+def visit_my_restaurant
+  add_restaurant_and_return
+  click_link 'My restaurant'
+end
