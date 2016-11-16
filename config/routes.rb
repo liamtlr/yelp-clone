5c4608538cba5ac05d2938ec2c3b58a29b59d466
Tomy8s <tomy8s@hotmail.com>
2016-11-16 16:48:43 +0000

    tried to implement facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # get '/', to: redirect('/restaurants')
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: "restaurants#index"
 
