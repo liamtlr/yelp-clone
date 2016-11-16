a93cba5ffd8a37fc43cdc68e30c77f4d56d140ad
Frances Maxwell <fmaxwell@gmail.com>
2016-11-16 10:38:22 +0000

    Installed Devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: redirect('/restaurants')
+  # get '/', to: redirect('/restaurants')
+
+  root to: "restaurants#index"
 
   resources :restaurants do
     resources :reviews
