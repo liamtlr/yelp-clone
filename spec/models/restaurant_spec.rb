4ffac905f7a510623791516eb288fd41f58b9802
Tomy8s <tomy8s@hotmail.com>
2016-11-16 09:46:04 +0000

    added validation to model layer

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
+  it 'is not valid with a name less than 3 chars' do
+    restaurant = Restaurant.create(name: 'a')
+    expect(restaurant).to have(1).error_on :name
+    expect(restaurant).not_to be_valid
+  end
+
+  it 'is not valid with an existing name' do
+    Restaurant.create(name: 'a restaurant')
+    restaurant = Restaurant.create(name: 'a restaurant')
+    expect(restaurant).to have(1).error_on :name
+    expect(restaurant).not_to be_valid
+  end
 end
