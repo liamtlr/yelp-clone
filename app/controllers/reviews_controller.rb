c2e1f54cd30ea2042a9c8beeb400f4acb9486e2f
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-18 08:52:01 +0000

    Experimented with ways to create reviewed_restaurants entry

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,8 +10,8 @@ class ReviewsController < ApplicationController
     @current_user = current_user
     @restaurant = Restaurant.find(params[:restaurant_id])
     @output = @restaurant.build_review_with_user_foreign_key(reviews_params, @current_user)
-    ReviewedRestaurant.create(user_id: current_user.id, restaurant_id: @restaurant.id)
-    puts @output
+    ReviewedRestaurant.create(user_id: @current_user.id, restaurant_id: @restaurant.id)
+    # reviewed_restaurants_output = @current_user.reviewed_restaurants.create({ id: @restaurant.id })
     redirect_to '/restaurants'
   end
 
